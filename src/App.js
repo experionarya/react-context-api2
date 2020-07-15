732a1a314420bd9e049155d486c5cae9da905dbb
Arya S Vijayan <arya.vijayan@experionglobal.local>
2020-07-15 14:12:39 +0530

    react context api with reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import './stylesheet/styles.css';
+import { Home } from './components/Home';
+import { AddEmployee } from './components/AddEmployee';
+import { EditEmployee } from './components/EditEmployee';
+
+
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/add" component={AddEmployee} exact />
+        <Route path="/edit/:id" component={EditEmployee} exact />
+      </Switch>
+      </BrowserRouter>
+    </GlobalProvider>
   );
 }
 
